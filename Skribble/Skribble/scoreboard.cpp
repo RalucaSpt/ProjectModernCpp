a5d247093706f97030450cff09f93f663f83da6f
ahtnonia <antoniamaria.stanila@gmail.com>
2023-10-29 14:29:26 +0200

    I created methods for Scoreboard class

---
+++ Skribble/Skribble/scoreboard.cpp
@@ -0,0 +1,20 @@
+﻿module scoreboard;
+
+import <iostream>;
+
+Scoreboard::Scoreboard() = default;
+
+void Scoreboard::addPlayer(const std::string& playerName) {
+    scores[playerName] = 0;
+}
+
+void Scoreboard::updateScore(const std::string& playerName, int points) {
+    if (scores.find(playerName) != scores.end()) {
+        scores[playerName] += points;
+    }
+    else {
+        std::cout << "Player not found." << std::endl;
+    }
+}
+
+
