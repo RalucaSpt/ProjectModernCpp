3d9a84fdfffb62c8b605d348d48461c8785b22cb
ahtnonia <antoniamaria.stanila@gmail.com>
2023-10-29 14:32:41 +0200

    I created methods for Scoreboard class(II)

--- Skribble/Skribble/scoreboard.cpp
+++ Skribble/Skribble/scoreboard.cpp
@@ -17,4 +17,21 @@ void Scoreboard::updateScore(const std::string& playerName, int points) {
     }
 }
 
+int Scoreboard::getScore(const std::string& playerName) const {
+    auto it = scores.find(playerName);
+    if (it != scores.end()) {
+        return it->second;
+    }
+    else {
+        std::cout << "Player not found." << std::endl;
+        return 0;
+    }
+}
+
+void Scoreboard::printScoreboard() const {
+    std::cout << "Scoreboard:" << std::endl;
+    for (const auto& [playerName, score] : scores) {
+        std::cout << playerName << ": " << score << std::endl;
+    }
+}
 
