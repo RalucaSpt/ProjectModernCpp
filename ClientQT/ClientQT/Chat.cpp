9c1625d5de716f4cce18f0ace4a0e35259967643
Antonia Stanila <antonimaria.stanila@gmail.com>
2023-12-10 13:01:48 +0200

    working on the chat class

--- ClientQT/ClientQT/Chat.cpp
+++ ClientQT/ClientQT/Chat.cpp
@@ -19,4 +19,28 @@ Chat::Chat(int port)
         std::cerr << "Bind failed.\n";
         exit(EXIT_FAILURE);
     }
+
+    // Ascult? conexiuni
+    if (listen(server_fd_, 3) < 0) {
+        std::cerr << "Listen failed.\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Chat::start()
+{
+    while (true) {
+        int new_socket;
+        struct sockaddr_in address;
+        int addrlen = sizeof(address);
+
+        // Accept? o nou? conexiune
+        if ((new_socket = accept(server_fd_, (struct sockaddr*)&address, (int*)&addrlen)) < 0) {
+            std::cerr << "Accept failed.\n";
+            continue;
+        }
+
+        // Trateaz? fiecare client într-un fir de execu?ie separat
+        std::thread(&Chat::handleClient, this, new_socket).detach();
+    }
 }
