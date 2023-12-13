616f5b015b15c8533a95fc1cf559d8243c569558
MilcuAlex <alexandru.milcu@student.untibv.ro>
2023-12-13 23:33:14 +0200

    Solving errors

--- Skribble/Skribble/player.cpp
+++ Skribble/Skribble/player.cpp
@@ -4,10 +4,10 @@ import <string>;
 
 using skribble::Player;
 
-Player::Player()
-	:m_idPlayer{ 0 }, m_score{ 0 }, m_isDrawing{ false }
-{
-
+//Player::Player()
+//	:m_idPlayer{ 0 }, m_score{ 0 }, m_isDrawing{ false }
+//{
+//}
 Player::Player(int IdPlayer, uint16_t score, const std::string& name)
 	:m_idPlayer { IdPlayer },
 	m_score{ score },
@@ -16,18 +16,17 @@ Player::Player(int IdPlayer, uint16_t score, const std::string& name)
 {
 
 }
-
-Player::Player(const Player& player)
-	:m_idPlayer{ player.m_idPlayer }, m_score{ player.m_score }, m_isDrawing{ player.m_isDrawing }
-{
-
-}
+//Player::Player(const Player& player)
+//	:m_idPlayer{ player.m_idPlayer }, m_score{ player.m_score }, m_isDrawing{ player.m_isDrawing }
+//{
+//
+//}
 
 Player::Player(int IdPlayer, const std::string& name,std::string password)
 	:m_idPlayer{ IdPlayer },
 	m_name{ name },
 	m_password{password},
-	m_drawingStatus{ DrawingStatus::NotDrawing }
+	m_isDrawing{ DrawingStatus::NotDrawing }
 {
 }
 
@@ -60,6 +59,24 @@ std::string Player::GetName() const
 {
 	return m_name;
 }
+std::string skribble::Player::GetPassword() const
+{
+	return m_password;
+}
+void Player::SetName(const std::string& name)
+{
+	m_name = name;
+}
+
+void Player::SetId(const int& id)
+{
+	m_idPlayer = id;
+}
+
+void skribble::Player::SetPassword(const std::string& password)
+{
+	m_password = password;
+}
 /*
 skribble::Player::DrawingStatus skribble::Player::GetIsDrawing() const
 {
@@ -67,10 +84,6 @@ skribble::Player::DrawingStatus skribble::Player::GetIsDrawing() const
 	return ceva;
 }
 
-std::string skribble::Player::GetPassword() const
-{
-	return m_password;
-}
 
 bool skribble::Player::guessedWord()
 {
@@ -82,24 +95,11 @@ void Player::SetIsDrawing(DrawingStatus isDrawing)
 	m_isDrawing = isDrawing;
 }
 
-void Player::SetName(const std::string& name)
-{
-	m_name = name;
-}
 
-void Player::SetId(const int& id) 
-{
-	m_idPlayer = id;
-}
-
-void skribble::Player::SetPassword(const std::string& password)
-{
-	m_password = password;
-}
 
 void Player::AddScore(uint16_t points, int nrPlayers)
 {
 	//
 }
-
+*/
 
