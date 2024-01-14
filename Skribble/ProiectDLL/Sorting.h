ccc6fce0fec092cdf6fcb94c299131ec354b923a
Durbaca Diana <diana.durbaca@student.unitbv.ro>
2024-01-14 22:02:29 +0200

    dll implementation

---
+++ Skribble/ProiectDLL/Sorting.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<vector>
+#include<algorithm>
+#include<ranges>
+#include <algorithm>
+#include <iostream>
+
+//extern "C++" {
+__declspec(dllexport) class sorting
+{
+public:
+    void SortPairs(std::vector<std::pair<int, std::string>>& input)
+    {
+        std::ranges::sort(input);
+    }
+    void test()
+    {
+        std::cout << "Hello Word";
+    }
+};
+
+
+//}
